dd2c2f79244c9b521df3062d24da22de19c1818a
Jenna Hines <jennahines@marketingadept.com>
2013-03-11 10:27:26 -0400

    adding rest of files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	$section = "";
+	$page = "";
+	$subsection = "";
+	$title = "";
+	$meta_description = "";
+	$meta_keywords = "";
+	include ("inc/header.php");
+?>
+
+
+
+
+
+<?php include("inc/footer.php"); ?>
