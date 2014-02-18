1aaebc5c218ffa52b98ce98065405d7262f83920
Jenna Hines <jennahines@marketingadept.com>
2014-02-18 14:52:47 -0500

    minor updates

--- inc/footer.php
+++ inc/footer.php
@@ -16,6 +16,14 @@
 	
 	<script src="<?php echo $root; ?>/js/plugins.js"></script>
 	<script src="<?php echo $root; ?>/js/main.js"></script>
-
+	
+	<!-- Google Analytics -->
+	<script>
+	    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+	    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	    s.parentNode.insertBefore(g,s)}(document,'script'));
+	</script>
+	
 </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -18,13 +18,6 @@
 	
 	<script src="<?php echo $root; ?>/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 	
-	<!-- Google Analytics -->
-	<script>
-	    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-	    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-	    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-	    s.parentNode.insertBefore(g,s)}(document,'script'));
-	</script>
 </head>
 <body>
 	<!--[if lt IE 7]>
